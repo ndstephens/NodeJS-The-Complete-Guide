b79e48a7e7eafcc8fff7c23915baeaf41a8e2d76
Nathaniel Stephens <ndstephens@gmail.com>
2019-03-11 16:49:43 -0400

    temp saving products, bit of refactoring

--- routes/admin.js
+++ routes/admin.js
@@ -3,17 +3,19 @@ const path = require('path')
 const express = require('express')
 const rootDir = require('../utils/rootDir')
 
-//?       /admin
+//? /admin
 
 const router = express.Router()
+const products = []
 
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 })
 
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body)
+  products.push({ title: req.body.title })
   res.redirect('/')
 })
 
-module.exports = router
+exports.adminRoutes = router
+exports.products = products
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,12 @@ const path = require('path')
 
 const express = require('express')
 const rootDir = require('../utils/rootDir')
+const { products } = require('./admin')
 
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
+  console.log('Shop.js:', products)
   res.sendFile(path.join(rootDir, 'views', 'shop.html'))
 })
 
