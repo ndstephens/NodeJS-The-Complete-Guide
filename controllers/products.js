37ddf2bf966bad186d33e56269bba1c7b4dd452d
Nathaniel Stephens <ndstephens@gmail.com>
2019-03-12 12:49:19 -0400

    add to navigation

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,7 @@ const Product = require('../models/product')
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/add-product', {
     pageTitle: 'Add Products',
-    activeTab: 'admin',
+    activeTab: 'admin-add',
   })
 }
 
