4ef9e6e9402365f90a32c026d3a101921b918d23
Nathaniel Stephens <ndstephens@gmail.com>
2019-03-13 19:51:00 -0400

    refactor cart model

--- models/cart.js
+++ models/cart.js
@@ -5,26 +5,44 @@ const rootDir = require('../utils/rootDir')
 
 const cartPath = path.join(rootDir, 'data', 'cart.json')
 
-module.exports = class Cart {
-  static addProduct(product) {
-    // Fetch previous cart
-    fs.readFile(cartPath, (err, data) => {
-      let cart = { products: [], totalPrice: 0 }
-      if (!err) {
-        cart = JSON.parse(data.toString())
+const getCartFromFile = cb => {
+  fs.readFile(cartPath, 'utf8', (err, data) => {
+    if (err) {
+      cb({ products: [], totalPrice: 0 })
+    } else {
+      if (!data) {
+        cb({ products: [], totalPrice: 0 })
+      } else {
+        cb(JSON.parse(data.toString()))
       }
+    }
+  })
+}
+
+module.exports = class Cart {
+  static updateTotalPrice(cart) {
+    return cart.products.reduce((total, prod) => {
+      return total + +prod.price * prod.qty
+    }, 0)
+  }
+
+  static addProduct({ id, price } = {}) {
+    getCartFromFile(cart => {
       // Analyze cart -> find existing product
-      const existingProduct = cart.products.find(p => p.id === product.id)
+      const existingProduct = cart.products.find(p => p.id === id)
       // Add or update product
       if (existingProduct) {
         existingProduct.qty++
       } else {
-        cart.products.push({ ...product, qty: 1 })
+        cart.products.push({
+          id,
+          price,
+          qty: 1,
+        })
       }
       // Update cart's totalPrice
-      cart.totalPrice = cart.products.reduce((total, prod) => {
-        return total + +prod.price * prod.qty
-      }, 0)
+      cart.totalPrice = this.updateTotalPrice(cart)
+
       // Update the cart JSON file
       fs.writeFile(cartPath, JSON.stringify(cart), err => console.log(err))
     })
