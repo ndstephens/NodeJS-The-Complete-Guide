2bedd55704d90345e4b4c0316f7b2b35f12849f5
Nathaniel Stephens <ndstephens@gmail.com>
2019-03-15 10:31:53 -0400

    create/sync models to db tables

--- 2_mySQL-version/app.js
+++ 2_mySQL-version/app.js
@@ -3,25 +3,34 @@ const path = require('path')
 
 const express = require('express')
 const { get404 } = require('./controllers/404')
+const sequelize = require('./utils/database')
 
 const shopRoutes = require('./routes/shop')
 const adminRoutes = require('./routes/admin')
 
-// INIT APP
+//* INIT APP
 const app = express()
+// Port
 const port = process.env.PORT || 3000
-app.set('view engine', 'ejs') // views directory set by default
+// View Engine
+app.set('view engine', 'ejs')
 
-// MIDDLEWARE
+//* MIDDLEWARE
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 
-// ROUTERS
+//* ROUTERS
 app.use('/', shopRoutes)
 app.use('/admin', adminRoutes)
 
-// 404 ERROR PAGE -- Catch All
+//* 404 ERROR PAGE -- Catch All
 app.use(get404)
 
-// RUN SERVER
-app.listen(port)
+//* RUN SERVER
+// Create/Sync database tables with the Models you created
+sequelize
+  .sync()
+  .then(result => {
+    app.listen(port)
+  })
+  .catch(err => console.log(err))
