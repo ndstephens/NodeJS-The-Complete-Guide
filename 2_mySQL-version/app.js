e44be040cc651d6d7b62929444d5cd76ab617770
Nathaniel Stephens <ndstephens@gmail.com>
2019-03-15 12:37:52 -0400

    adding One-To-Many relationship

--- 2_mySQL-version/app.js
+++ 2_mySQL-version/app.js
@@ -5,10 +5,18 @@ const express = require('express')
 const { get404 } = require('./controllers/404')
 const sequelize = require('./utils/database')
 
+//? ROUTES
 const shopRoutes = require('./routes/shop')
 const adminRoutes = require('./routes/admin')
 
-//* INIT APP
+//? MODELS
+const Product = require('./models/product')
+const User = require('./models/user')
+
+//*--------------------------------------------------/
+//*           INITIALIZE APP
+//*--------------------------------------------------/
+// Express App
 const app = express()
 // Port
 const port = process.env.PORT || 3000
@@ -26,11 +34,15 @@ app.use('/admin', adminRoutes)
 //* 404 ERROR PAGE -- Catch All
 app.use(get404)
 
+//* ASSOCIATE TABLES/MODELS IN DB
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' })
+User.hasMany(Product)
+
 //* RUN SERVER
 // Create/Sync database tables with the Models you created
 sequelize
-  .sync()
-  .then(result => {
+  .sync({ force: true })
+  .then(() => {
     app.listen(port)
   })
   .catch(err => console.log(err))
