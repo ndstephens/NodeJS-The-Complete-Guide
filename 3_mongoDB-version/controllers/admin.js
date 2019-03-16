bd6e513848f4b8ea232def00909338fdc0016ff9
Nathaniel Stephens <ndstephens@gmail.com>
2019-03-16 14:05:06 -0400

    update product and display update

--- 3_mongoDB-version/controllers/admin.js
+++ 3_mongoDB-version/controllers/admin.js
@@ -13,8 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const product = new Product(req.body)
   product
     .save()
-    .then(result => {
-      console.log(result)
+    .then(() => {
       res.redirect('/admin/list-products')
     })
     .catch(err => console.log(err))
@@ -32,35 +31,26 @@ exports.getListProducts = (req, res, next) => {
     .catch(err => console.log(err))
 }
 
-// exports.getEditProduct = (req, res, next) => {
-//   // Product.findByPk(req.params.productId)
-//   req.user
-//     .getProducts({ where: { id: req.params.productId } })
-//     .then(([product]) => {
-//       res.render('admin/edit-product', {
-//         pageTitle: 'Admin Edit Product',
-//         activeTab: 'admin-edit',
-//         editMode: true,
-//         product,
-//       })
-//     })
-//     .catch(err => console.log(err))
-// }
+exports.getEditProduct = (req, res, next) => {
+  Product.findById(req.params.productId)
+    .then(product => {
+      res.render('admin/edit-product', {
+        pageTitle: 'Admin Edit Product',
+        activeTab: 'admin-edit',
+        editMode: true,
+        product,
+      })
+    })
+    .catch(err => console.log(err))
+}
 
-// exports.postEditProduct = (req, res, next) => {
-//   const { id, title, price, imageUrl, description } = req.body
-//   Product.update(
-//     {
-//       title: title.trim(),
-//       price: parseFloat(price).toFixed(2) || 0.0,
-//       description: description.trim(),
-//       imageUrl: imageUrl || 'https://picsum.photos/300/300/?random',
-//     },
-//     { where: { id } }
-//   )
-//     .then(() => res.redirect('/admin/list-products'))
-//     .catch(err => console.log(err))
-// }
+exports.postEditProduct = (req, res, next) => {
+  const editedProduct = new Product(req.body)
+  editedProduct
+    .save()
+    .then(() => res.redirect('/admin/list-products'))
+    .catch(err => console.log(err))
+}
 
 // exports.postDeleteProduct = (req, res, next) => {
 //   // Product.findByPk(req.body.id)
--- 3_mongoDB-version/models/product.js
+++ 3_mongoDB-version/models/product.js
@@ -2,16 +2,26 @@ const mongodb = require('mongodb')
 const { getDb } = require('../utils/database')
 
 class Product {
-  constructor({ title, price, description, imageUrl } = {}) {
+  constructor({ title, price, description, imageUrl, id } = {}) {
     this.title = title.trim()
     this.price = parseFloat(price).toFixed(2) || 0.0
     this.description = description.trim()
     this.imageUrl = imageUrl || 'https://picsum.photos/300/300/?random'
+    this._id = id
   }
 
   save() {
     const db = getDb()
-    return db.collection('products').insertOne(this)
+    if (this._id) {
+      // UPDATE PRODUCT
+      const { _id, ...rest } = this
+      return db
+        .collection('products')
+        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: rest })
+    } else {
+      // CREATE NEW PRODUCT
+      return db.collection('products').insertOne(this)
+    }
   }
 
   static fetchall() {
