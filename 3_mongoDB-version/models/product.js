c7aa2d50470e63dc16c22fa119fb646edae1cefc
Nathaniel Stephens <ndstephens@gmail.com>
2019-03-16 14:32:09 -0400

    refactor to adding a new product

--- 3_mongoDB-version/models/product.js
+++ 3_mongoDB-version/models/product.js
@@ -2,12 +2,12 @@ const mongodb = require('mongodb')
 const { getDb } = require('../utils/database')
 
 class Product {
-  constructor({ title, price, description, imageUrl, id = undefined } = {}) {
+  constructor({ title, price, description, imageUrl, id } = {}) {
     this.title = title.trim()
     this.price = parseFloat(price).toFixed(2) || 0.0
     this.description = description.trim()
     this.imageUrl = imageUrl || 'https://picsum.photos/300/300/?random'
-    this._id = id
+    this._id = id ? new mongodb.ObjectId(id) : null
   }
 
   save() {
@@ -15,9 +15,7 @@ class Product {
     if (this._id) {
       // UPDATE PRODUCT
       const { _id, ...rest } = this
-      return db
-        .collection('products')
-        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: rest })
+      return db.collection('products').updateOne({ _id }, { $set: rest })
     } else {
       // CREATE NEW PRODUCT
       return db.collection('products').insertOne(this)
