ca8efa59d32ff69ad2094581d299da96a760793b
Nathaniel Stephens <ndstephens@gmail.com>
2019-03-17 16:21:39 -0400

    create product schema

--- 4_mongoose-version/models/product.js
+++ 4_mongoose-version/models/product.js
@@ -1,50 +1,73 @@
-const mongodb = require('mongodb')
-const { getDb } = require('../utils/database')
+const mongoose = require('mongoose')
 
-class Product {
-  constructor({ title, price, description, imageUrl, id, userId } = {}) {
-    this._id = id ? new mongodb.ObjectId(id) : null
-    this.title = title.trim()
-    this.price = parseFloat(price).toFixed(2) || 0.0
-    this.description = description.trim()
-    this.imageUrl = imageUrl || 'https://picsum.photos/300/300/?random'
-    this.userId = userId
-  }
+const Schema = mongoose.Schema
 
-  save() {
-    const db = getDb()
-    if (this._id) {
-      // UPDATE PRODUCT
-      const { _id, ...rest } = this
-      return db.collection('products').updateOne({ _id }, { $set: rest })
-    } else {
-      // CREATE NEW PRODUCT
-      return db.collection('products').insertOne(this)
-    }
-  }
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+})
 
-  static fetchall() {
-    const db = getDb()
-    return db
-      .collection('products')
-      .find()
-      .toArray()
-  }
+// const mongodb = require('mongodb')
+// const { getDb } = require('../utils/database')
 
-  static findById(id) {
-    const db = getDb()
-    return db
-      .collection('products')
-      .find({ _id: new mongodb.ObjectId(id) })
-      .next()
-  }
+// class Product {
+//   constructor({ title, price, description, imageUrl, id, userId } = {}) {
+//     this._id = id ? new mongodb.ObjectId(id) : null
+//     this.title = title.trim()
+//     this.price = parseFloat(price).toFixed(2) || 0.0
+//     this.description = description.trim()
+//     this.imageUrl = imageUrl || 'https://picsum.photos/300/300/?random'
+//     this.userId = userId
+//   }
 
-  static deleteById(id) {
-    const db = getDb()
-    return db
-      .collection('products')
-      .deleteOne({ _id: new mongodb.ObjectId(id) })
-  }
-}
+//   save() {
+//     const db = getDb()
+//     if (this._id) {
+//       // UPDATE PRODUCT
+//       const { _id, ...rest } = this
+//       return db.collection('products').updateOne({ _id }, { $set: rest })
+//     } else {
+//       // CREATE NEW PRODUCT
+//       return db.collection('products').insertOne(this)
+//     }
+//   }
 
-module.exports = Product
+//   static fetchall() {
+//     const db = getDb()
+//     return db
+//       .collection('products')
+//       .find()
+//       .toArray()
+//   }
+
+//   static findById(id) {
+//     const db = getDb()
+//     return db
+//       .collection('products')
+//       .find({ _id: new mongodb.ObjectId(id) })
+//       .next()
+//   }
+
+//   static deleteById(id) {
+//     const db = getDb()
+//     return db
+//       .collection('products')
+//       .deleteOne({ _id: new mongodb.ObjectId(id) })
+//   }
+// }
+
+// module.exports = Product
