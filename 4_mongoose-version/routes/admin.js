ae96014eda558dd76aa22710d1d3525c6d728be5
Nathaniel Stephens <ndstephens@gmail.com>
2019-03-17 16:09:26 -0400

    comment out routes

--- 4_mongoose-version/routes/admin.js
+++ 4_mongoose-version/routes/admin.js
@@ -1,27 +1,27 @@
 const express = require('express')
 
 const {
-  getAddProduct,
-  postAddProduct,
-  getListProducts,
-  getEditProduct,
-  postEditProduct,
-  postDeleteProduct,
+  // getAddProduct,
+  // postAddProduct,
+  // getListProducts,
+  // getEditProduct,
+  // postEditProduct,
+  // postDeleteProduct,
 } = require('../controllers/admin')
 
 //? ------ '/admin'
 const router = express.Router()
 
-router.get('/add-product', getAddProduct)
+// router.get('/add-product', getAddProduct)
 
-router.post('/add-product', postAddProduct)
+// router.post('/add-product', postAddProduct)
 
-router.get('/list-products', getListProducts)
+// router.get('/list-products', getListProducts)
 
-router.get('/edit-product/:productId', getEditProduct)
+// router.get('/edit-product/:productId', getEditProduct)
 
-router.post('/edit-product', postEditProduct)
+// router.post('/edit-product', postEditProduct)
 
-router.post('/delete-product', postDeleteProduct)
+// router.post('/delete-product', postDeleteProduct)
 
 module.exports = router
--- 4_mongoose-version/routes/shop.js
+++ 4_mongoose-version/routes/shop.js
@@ -1,35 +1,35 @@
 const express = require('express')
 
 const {
-  getIndex,
-  getProducts,
-  getProduct,
-  getCart,
-  postCart,
-  postCartDeleteItem,
-  getOrders,
-  postOrder,
+  // getIndex,
+  // getProducts,
+  // getProduct,
+  // getCart,
+  // postCart,
+  // postCartDeleteItem,
+  // getOrders,
+  // postOrder,
   // getCheckout,
 } = require('../controllers/shop')
 
 //? ------ '/'
 const router = express.Router()
 
-router.get('/', getIndex)
+// router.get('/', getIndex)
 
-router.get('/products', getProducts)
+// router.get('/products', getProducts)
 
-router.get('/products/:productId', getProduct)
+// router.get('/products/:productId', getProduct)
 
-router.get('/cart', getCart)
+// router.get('/cart', getCart)
 
-router.post('/cart', postCart)
+// router.post('/cart', postCart)
 
-router.post('/cart-delete-item', postCartDeleteItem)
+// router.post('/cart-delete-item', postCartDeleteItem)
 
-router.get('/orders', getOrders)
+// router.get('/orders', getOrders)
 
-router.post('/create-order', postOrder)
+// router.post('/create-order', postOrder)
 
 // router.get('/checkout', getCheckout)
 
