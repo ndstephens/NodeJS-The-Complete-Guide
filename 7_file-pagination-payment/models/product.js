62c26dd195cd4a7538f7f0e946b7dc87e63429db
Nathaniel Stephens <ndstephens@gmail.com>
2019-03-23 12:38:34 -0400

    refactor and clean up

--- 7_file-pagination-payment/models/product.js
+++ 7_file-pagination-payment/models/product.js
@@ -11,7 +11,7 @@ const productSchema = new Schema(
     },
     price: {
       type: Number,
-      default: 0,
+      required: true,
     },
     description: {
       type: String,
@@ -20,7 +20,7 @@ const productSchema = new Schema(
     },
     imageUrl: {
       type: String,
-      default: 'https://picsum.photos/300/300/?random',
+      required: true,
     },
     userId: {
       type: mongoose.Schema.Types.ObjectId,
@@ -35,54 +35,3 @@ const productSchema = new Schema(
 
 module.exports = mongoose.model('Product', productSchema)
 //? the model name, 'Product', will be used by mongoose to create a collection named 'products'
-
-// const mongodb = require('mongodb')
-// const { getDb } = require('../utils/database')
-
-// class Product {
-//   constructor({ title, price, description, imageUrl, id, userId } = {}) {
-//     this._id = id ? new mongodb.ObjectId(id) : null
-//     this.title = title.trim()
-//     this.price = parseFloat(price).toFixed(2) || 0.0
-//     this.description = description.trim()
-//     this.imageUrl = imageUrl || 'https://picsum.photos/300/300/?random'
-//     this.userId = userId
-//   }
-
-//   save() {
-//     const db = getDb()
-//     if (this._id) {
-//       // UPDATE PRODUCT
-//       const { _id, ...rest } = this
-//       return db.collection('products').updateOne({ _id }, { $set: rest })
-//     } else {
-//       // CREATE NEW PRODUCT
-//       return db.collection('products').insertOne(this)
-//     }
-//   }
-
-//   static fetchall() {
-//     const db = getDb()
-//     return db
-//       .collection('products')
-//       .find()
-//       .toArray()
-//   }
-
-//   static findById(id) {
-//     const db = getDb()
-//     return db
-//       .collection('products')
-//       .find({ _id: new mongodb.ObjectId(id) })
-//       .next()
-//   }
-
-//   static deleteById(id) {
-//     const db = getDb()
-//     return db
-//       .collection('products')
-//       .deleteOne({ _id: new mongodb.ObjectId(id) })
-//   }
-// }
-
-// module.exports = Product
