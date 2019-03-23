3a83491c380fa4bf5a1d460bbce446106dfe5f7d
Nathaniel Stephens <ndstephens@gmail.com>
2019-03-23 15:18:15 -0400

    fix CORS issues

--- 8_REST-API-basics/app.js
+++ 8_REST-API-basics/app.js
@@ -7,7 +7,14 @@ const feedRoutes = require('./routes/feed')
 const app = express()
 
 //* MIDDLEWARE
-app.use(express.json())
+app.use(express.json()) // application/json
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // instead of '*' can list specific domains
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next()
+}) //? FIX CORS ISSUES
 
 //* INIT ROUTES
 app.use('/feed', feedRoutes)
