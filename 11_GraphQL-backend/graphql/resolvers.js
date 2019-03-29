31fb79d3c3bb2aaccf4149e8a35f16628882f923
Nathaniel Stephens <ndstephens@gmail.com>
2019-03-29 13:21:44 -0400

    create post mutation

--- 11_GraphQL-backend/graphql/resolvers.js
+++ 11_GraphQL-backend/graphql/resolvers.js
@@ -3,9 +3,12 @@ const validator = require('validator')
 const jwt = require('jsonwebtoken')
 
 const User = require('../models/user')
+const Post = require('../models/post')
 
+//? CREATE USER
 exports.createUser = async (args, req) => {
   const { name, email, password } = args.userInput
+
   const errors = []
   if (!validator.isEmail(email)) {
     errors.push({ message: 'Email is invalid' })
@@ -36,12 +39,13 @@ exports.createUser = async (args, req) => {
     email,
     password: hashedPw,
   })
-
   const createdUser = await user.save()
 
   return { ...createdUser._doc, _id: createdUser._id.toString() }
 }
 
+//
+//? LOGIN
 exports.login = async ({ email, password }) => {
   const user = await User.findOne({ email })
   if (!user) {
@@ -67,4 +71,41 @@ exports.login = async ({ email, password }) => {
   )
 
   return { token, userId: user._id.toString() }
+}
+
+//
+//? CREATE POST
+exports.createPost = async (args, req) => {
+  const { title, content, imageUrl } = args.postInput
+
+  const errors = []
+  if (validator.isEmpty(title) || !validator.isLength(title, { min: 5 })) {
+    errors.push({ message: 'Title is invalid' })
+  }
+  if (validator.isEmpty(content) || !validator.isLength(content, { min: 5 })) {
+    errors.push({ message: 'Content is invalid' })
+  }
+
+  if (errors.length > 0) {
+    const error = new Error('Invalid input')
+    error.data = errors
+    error.code = 422
+    throw error
+  }
+
+  const post = new Post({
+    title,
+    content,
+    imageUrl,
+  })
+  const createdPost = await post.save()
+
+  // add post to user's posts
+
+  return {
+    ...createdPost._doc,
+    _id: createdPost._id.toString(),
+    createdAt: createdPost.createdAt.toISOString(),
+    updatedAt: createdPost.updatedAt.toISOString(),
+  }
 }
