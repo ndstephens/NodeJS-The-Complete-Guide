34591df7e7676cd07922c9e8d7d5f9689ecaa099
Nathaniel Stephens <ndstephens@gmail.com>
2019-03-29 10:04:41 -0400

    mutation schema

--- 11_GraphQL-backend/graphql/schema.js
+++ 11_GraphQL-backend/graphql/schema.js
@@ -1,16 +1,36 @@
 const { buildSchema } = require('graphql')
 
 module.exports = buildSchema(`
-  type TestData {
-    text: String!
-    views: Int!
+  type Post {
+    _id: ID!
+    title: String!
+    content: String!
+    imageUrl: String!
+    creator: User!
+    createdAt: String!
+    updatedAt: String!
   }
 
-  type RootQuery {
-    hello: TestData!
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+    password: String
+    status: String!
+    post: [Post!]!
+  }
+
+  input UserInputData {
+    name: String!
+    email: String!
+    password: String!
+  }
+
+  type RootMutation {
+    createUser(userInput: UserInputData!): User!
   }
 
   schema {
-      query: RootQuery
+    mutation: RootMutation
   }
 `)
