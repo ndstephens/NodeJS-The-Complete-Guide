aa309f4b5a953152bbde745385076a8539f537bd
Nathaniel Stephens <ndstephens@gmail.com>
2019-03-29 11:17:50 -0400

    validation

--- 11_GraphQL-backend/graphql/resolvers.js
+++ 11_GraphQL-backend/graphql/resolvers.js
@@ -1,10 +1,25 @@
 const bcrypt = require('bcryptjs')
+const validator = require('validator')
 
 const User = require('../models/user')
 
 module.exports = {
   async createUser(args, req) {
     const { name, email, password } = args.userInput
+    const errors = []
+    if (!validator.isEmail(email)) {
+      errors.push({ message: 'Email is invalid' })
+    }
+    if (
+      validator.isEmpty(password) ||
+      !validator.isLength(password, { min: 5 })
+    ) {
+      errors.push({ message: 'Password too short' })
+    }
+
+    if (errors.length > 0) {
+      throw new Error('Invalid input')
+    }
 
     const isExistingUser = await User.findOne({ email })
     if (isExistingUser) {
