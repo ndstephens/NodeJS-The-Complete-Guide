2c04ddb97e3095f1be4f0bf5bb2874276ff69785
Nathaniel Stephens <ndstephens@gmail.com>
2019-03-30 12:53:24 -0400

    fix pagination bug

--- 12_GraphQL-frontend/src/pages/Feed/Feed.js
+++ 12_GraphQL-frontend/src/pages/Feed/Feed.js
@@ -267,12 +267,14 @@ class Feed extends Component {
         }
         this.setState(prevState => {
           let updatedPosts = [...prevState.posts]
+          let updatedTotalPosts = prevState.totalPosts
           if (prevState.editPost) {
             const postIndex = prevState.posts.findIndex(
               p => p._id === prevState.editPost._id
             )
             updatedPosts[postIndex] = post
           } else {
+            updatedTotalPosts++
             if (prevState.posts.length >= 2) {
               updatedPosts.pop()
             }
@@ -283,6 +285,7 @@ class Feed extends Component {
             isEditing: false,
             editPost: null,
             editLoading: false,
+            totalPosts: updatedTotalPosts,
           }
         })
       })
